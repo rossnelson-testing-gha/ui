a85a6e34ddc7a519bfe37cf0234ed7149ac7ae11
Ross Nelson <axcess1@me.com>
2025-05-09 10:32:14 -0400

    update

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -2,7 +2,7 @@
 	export let title = "";
 	export let content = "";
 	export let image = "";
-	export let funFact = "This is a really fun card!";
+	export let funFact = "This is a really not fun card!";
 </script>
 
 <div class="card">
